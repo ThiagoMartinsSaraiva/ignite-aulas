32faaf462b9fad0e888e81bdae21554af76bd1dd
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-03-23 02:08:27 -0300

    utilizando sass

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -33,7 +33,12 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader','sass-loader'],
+      },
     ],
   }
 }
