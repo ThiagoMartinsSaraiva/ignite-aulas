4b6bf205cdc9c6dc2729b173533b3a8ebd62ea1b
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-03-23 01:35:24 -0300

    estrutura do reactjs

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,6 +1,7 @@
 const { resolve } = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: resolve(__dirname, 'dist'),
